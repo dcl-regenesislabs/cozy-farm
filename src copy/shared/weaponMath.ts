1d89a461a111181e97f1dd4a675a6f8889f2753e
Giorgio <65674128+Gaffomdz@users.noreply.github.com>
2026-04-17 10:13:24 +0200

    rfis: remove-ia-context

--- src copy/shared/weaponMath.ts
+++
@@ -1,8 +0,0 @@
-import { Quaternion } from '@dcl/sdk/math'
-
-export function getLocalRotationFromWorld(parentRotation: Quaternion, worldRotation: Quaternion): Quaternion {
-  return Quaternion.multiply(
-    Quaternion.create(-parentRotation.x, -parentRotation.y, -parentRotation.z, parentRotation.w),
-    worldRotation
-  )
-}
